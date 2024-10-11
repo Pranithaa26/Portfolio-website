80d8d8f3766d5f18dd37836f532c6378fd7071a1
Pranithaa26 <kommaapranithaa@gmail.com>
2024-10-11 15:32:06 +0530

    "Initial commit with HTML, CSS, and JS files"

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to open the modal
+function openModal() {
+    document.getElementById('contactModal').style.display = 'flex';
+}
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById('contactModal').style.display = 'none';
+}
